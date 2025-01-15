74a9bab49691a7fcc913b4443f6424d1f6a2b045
bakyfb <bakyurayew2@gmail.com>
2025-01-15 16:31:22 +0500

    first commit

---
+++ database/migrations/2025_01_10_161831_create_music_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->unsignedInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artist');
+            $table->unsignedInteger('album_id');
+            $table->foreign('album_id')->references('id')->on('album')->nullable();
+            $table->string('name');
+            $table->string('audio')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('music');
+    }
+};
